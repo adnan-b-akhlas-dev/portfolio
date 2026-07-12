55a948fb7522fc88b29f97f00810ee3b527d1b4b
adnan-b-akhlas-dev <adnan.b.akhlas.dev@gmail.com>
2026-07-13 02:13:06 +0600

    feat: add mail verification on contact form

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,9 +1,10 @@
 const env = {
-  smtp_host: process.env.SMTP_HOST,
-  smtp_port: process.env.SMTP_PORT,
-  smtp_user: process.env.SMTP_USER,
-  smtp_pass: process.env.SMTP_PASS,
-  admin_email: process.env.ADMIN_EMAIL,
+  smtp_host: process.env.SMTP_HOST as string,
+  smtp_port: process.env.SMTP_PORT as string,
+  smtp_user: process.env.SMTP_USER as string,
+  smtp_pass: process.env.SMTP_PASS as string,
+  admin_email: process.env.ADMIN_EMAIL as string,
+  mailboxlayer_api_key: process.env.MAILBOXLAYER_API_KEY as string,
 };
 
 export default env;
